f3cdc63f7bb083092b018a314aef945bff969399
Victor Queiros <95942380+JvQueiros@users.noreply.github.com>
2026-01-06 14:17:43 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,10 +9,10 @@ sudo systemctl start docker
 sudo systemctl enable docker
 
 # Download da imagem do DockerHub
-docker pull asasgroup/garantias_ia:latest
+docker pull image:latest
 
 # Criação da rede e dos containers sem o compose
 docker network create gpt_parsing_default
 docker run -d --name redis --network gpt_parsing_default -p 6379:6379 redis:7
-docker run -d --name listener --network gpt_parsing_default -p 8000:8000 asasgroup/garantias_ia:latest
-docker run -d --name celery_worker1 --network gpt_parsing_default asasgroup/garantias_ia:latest celery -A tasks worker --loglevel=info
+docker run -d --name listener --network gpt_parsing_default -p 8000:8000 image:latest
+docker run -d --name celery_worker1 --network gpt_parsing_default image:latest celery -A tasks worker --loglevel=info
